93ca009f56e64eb02492554d6765c33d9bb469c3
Sbonga Mabaso <mabasos@wavesoft.co.za>
2023-01-06 15:18:02 +0200

    adding text style

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:travelsa/screens/location/location.dart';
+import 'package:travelsa/style.dart';
+// import 'style.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: LocationScreen());
+    return MaterialApp(
+        home: const LocationScreen(),
+        theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+              // ignore: deprecated_member_use
+              textTheme: TextTheme(title: AppBarTextStyle),
+            ),
+            textTheme: TextTheme(
+              title: TitleTextStyle,
+              body1: Body1TextStyle,
+            )));
   }
 }
